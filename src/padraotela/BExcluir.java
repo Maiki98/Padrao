4e82bf0847497fc37f8d416c4b4f179ad4794b3e
Maiki <125501475+Maiki98@users.noreply.github.com>
2023-08-26 23:01:55 -0300

    Add files via upload

---
+++ src/padraotela/BExcluir.java
@@ -0,0 +1,14 @@
+package padraotela;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JButton;
+
+public class BExcluir extends JButton {
+
+    public BExcluir() {
+        this.setText("EXCLUIR");
+        this.setPreferredSize(new Dimension(110, 30));
+        this.setBackground(Color.red);
+    }
+}
